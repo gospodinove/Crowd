981149cd02f06faf27c2ea31316fa8fc09241fb2
Emanuil Gospodinov <gospodinove@gmail.com>
2021-12-26 18:18:40 +0200

    [#24] - Format dates with device locale and timezone in mind

---
+++ js/reducers/native.ts
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+type StateT = {}
+
+const initialState: StateT = {}
+
+export const nativeSlice = createSlice({
+  name: 'native',
+  initialState,
+  reducers: {}
+})
