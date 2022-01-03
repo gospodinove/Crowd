8b1345323717f1c22b5bfa2a92589e7653248250
Emanuil Gospodinov <gospodinove@gmail.com>
2022-01-03 14:19:12 +0200

    [#40] - Memoize notifications tab

--- js/components/NotificationsScreen.tsx
+++ js/components/NotificationsScreen.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { memo } from 'react'
 import { View } from 'react-native'
 
 const NotificationsScreen = () => {
   return <View></View>
 }
 
-export default NotificationsScreen
+export default memo(NotificationsScreen)
