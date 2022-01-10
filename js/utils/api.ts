afa2b9c788e47f7bd6dc7d013ed3ca12c2054441
Emanuil Gospodinov <gospodinove@gmail.com>
2022-01-10 22:12:24 +0200

    [#46] - RecordToUnion type

--- js/utils/api.ts
+++ js/utils/api.ts
@@ -1,9 +1,21 @@
 import auth from '@react-native-firebase/auth'
 import firestore from '@react-native-firebase/firestore'
-import ApiCallT from '../types/ApiCall'
+import ApiT from '../types/Api'
 import { assertNever } from './assertNever'
 
-export default function api(call: ApiCallT) {
+// 1. new type that translates the ApiT to a Record of some sort
+// 2. helper function that translates the params of current api() to the new Record
+// 3. private function that has the logic of the current api()
+
+// makes a mapped type {key1: value1, key2: value2}
+// into {type: key1, params: value1} | {type: key2, params: value2}
+type RecordToUnion<Type> = {
+  [Key in keyof Type]: { type: Key; params: Type[Key] }
+}[keyof Type]
+
+// eventually move to something like this
+// const api = <K extends keyof ApiT>(name: K, params: ApiT[K]) => {
+export default function api(call: RecordToUnion<ApiT>) {
   switch (call.type) {
     case 'signInWithEmailAndPassword':
       return () =>
